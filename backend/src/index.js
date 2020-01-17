ff872996828d4f55d00c37870846568eec77858a
willian <wopetri11@gmail.com>
2020-01-17 12:56:17 -0400

    Semana OmniStack finalizado com sucesso

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const http = require('http');
 const cors = require('cors');
+const { setupWebsocket } = require ('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ul6lf.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -17,4 +22,4 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
